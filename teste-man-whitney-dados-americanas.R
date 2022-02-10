ea5891454325d7a81aba4b4db8cc2408e69b0f1e
Guilherme Ciconi <55059239+ciconig@users.noreply.github.com>
2022-02-09 22:59:36 -0300

    Update teste-man-whitney-dados-americanas.R

--- teste-man-whitney-dados-americanas.R
+++ teste-man-whitney-dados-americanas.R
@@ -14,18 +14,24 @@ dados$Departamento = factor(dados$Departamento)
 wilcox.test(Custo_Mkt ~ Departamento, dados)
 
 # histograma
+p# histograma
 par(mfrow=c(2,2))
 hist(dados$Custo_Mkt[dados$Departamento == "Bolas de Gude"],
-     ylab="Frequência", xlab="Custo MKT", main="Bolas de Gude")
+     ylab="FrequÃªncia", xlab="Custo MKT", main="Bolas de Gude", probability = T)
+lines(density(dados$Custo_Mkt[dados$Departamento == "Bolas de Gude"]))
+med = median(dados$Custo_Mkt[dados$Departamento == "Bolas de Gude"])
+abline(v = med, col = 'blue')
 hist(dados$Custo_Mkt[dados$Departamento == "Cadeiras"],
-     ylab="Frequência", xlab="Custo MKT", main="Cadeiras")
-
+     ylab="FrequÃªncia", xlab="Custo MKT", main="Cadeiras", probability = T)
+lines(density(dados$Custo_Mkt[dados$Departamento == "Cadeiras"]))
+med = median(dados$Custo_Mkt[dados$Departamento == "Cadeiras"])
+abline(v = med, col = 'blue')
 # boxplot
 #par(mfrow=c(1,2))
 boxplot(dados$Custo_Mkt[dados$Departamento == "Bolas de Gude"],
-        ylab="Frequência", xlab="Custo MKT", main="Bolas de Gude")
+        ylab="FrequÃªncia", xlab="Custo MKT", main="Bolas de Gude")
 boxplot(dados$Custo_Mkt[dados$Departamento == "Cadeiras"],
-        ylab="Frequência", xlab="Custo MKT", main="Cadeiras")
+        ylab="FrequÃªncia", xlab="Custo MKT", main="Cadeiras")
 
 #
 wilcox.test(Desconto_Produto ~ Departamento, dados)
@@ -35,9 +41,9 @@ wilcox.test(Desconto_Produto ~ Departamento, dados)
 par(mfrow=c(1,2))
 
 hist(dados$Desconto_Produto[dados$Departamento == "Bolas de Gude"],
-     ylab="Frequência", xlab="Desconto_Produto", main="Bolas de Gude")
+     ylab="FrequÃªncia", xlab="Desconto_Produto", main="Bolas de Gude")
 hist(dados$Desconto_Produto[dados$Departamento == "Cadeiras"],
-     ylab="Frequência", xlab="Desconto_Produto", main="Cadeiras")
+     ylab="FrequÃªncia", xlab="Desconto_Produto", main="Cadeiras")
 
 #
 wilcox.test(Faturamento_Produto ~ Departamento, dados)
@@ -45,9 +51,9 @@ wilcox.test(Faturamento_Produto ~ Departamento, dados)
 par(mfrow=c(1,2))
 
 hist(dados$Faturamento_Produto[dados$Departamento == "Bolas de Gude"],
-     ylab="Frequência", xlab="Faturamento_Produto", main="Bolas de Gude")
+     ylab="FrequÃªncia", xlab="Faturamento_Produto", main="Bolas de Gude")
 hist(dados$Faturamento_Produto[dados$Departamento == "Cadeiras"],
-     ylab="Frequência", xlab="Faturamento_Produto", main="Cadeiras")
+     ylab="FrequÃªncia", xlab="Faturamento_Produto", main="Cadeiras")
 
 #
 wilcox.test(Faturamento_Frete ~ Departamento, dados)
@@ -55,16 +61,16 @@ wilcox.test(Faturamento_Frete ~ Departamento, dados)
 par(mfrow=c(1,2))
 
 hist(dados$Faturamento_Frete[dados$Departamento == "Bolas de Gude"],
-     ylab="Frequência", xlab="Faturamento_Frete", main="Bolas de Gude")
+     ylab="FrequÃªncia", xlab="Faturamento_Frete", main="Bolas de Gude")
 hist(dados$Faturamento_Frete[dados$Departamento == "Cadeiras"],
-     ylab="Frequência", xlab="Faturamento_Frete", main="Cadeiras")
+     ylab="FrequÃªncia", xlab="Faturamento_Frete", main="Cadeiras")
 
-####################### Interpretação do teste #################################
+####################### InterpretaÃ§Ã£o do teste #################################
 
 # p-valor > 0.05 -> h0: mediana grupo A = mediana grupo B
 # p-valor <= 0.05 -> h1: mediana grupo A != mediana grupo B
 
-####################### Análise descritiva dos dados ###########################
+####################### AnÃ¡lise descritiva dos dados ###########################
 
 dados %>% group_by(Departamento) %>% 
   get_summary_stats(Custo_Mkt, Desconto_Produto, Faturamento_Produto, 
